11d6ee77f36432f859bd4141041f9dcee7bc61f6
Gianluca Riggio <luca@maximumawesome.nl>
2023-04-29 23:28:47 +0200

    Ensure we get the filename and line number for errors and warnings

--- src/Support/Error.php
+++ src/Support/Error.php
@@ -2,6 +2,7 @@
 
 namespace Vagebnd\EnvatoThemecheckCli\Support;
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Vagebnd\EnvatoThemecheckCli\Enums\ErrorLevel;
 use voku\helper\HtmlDomParser;
@@ -14,6 +15,10 @@ class Error
 
     protected string $message;
 
+    protected string|null $file = null;
+
+    protected int|null $line = null;
+
     public static function parse(string $error)
     {
         return (new static($error))->parseError(
@@ -21,12 +26,22 @@ class Error
         );
     }
 
-    private function parseError(HtmlDomParser $dom, ?string $filename = null)
+    private function parseError(HtmlDomParser $dom)
     {
         $level = $dom->find('.tc-required,.tc-warning,.tc-info', 0)->text;
 
         $this->level = ErrorLevel::fromString($level);
-        $this->message = trim(Str::after($dom->text(), "$level:"));
+        $this->message = html_entity_decode(trim(Str::after($dom->text(), "$level:")));
+
+        // Check if the error has a file
+        if (preg_match('/\bfile\s+(\S+)/i', $this->message, $matches)) {
+            $this->file = Arr::get($matches, 1);
+        }
+
+        // Check if the error has a line number
+        if (preg_match('/\bline\s+(\d+)/i', $this->message, $matches)) {
+            $this->line = Arr::get($matches, 1);
+        }
 
         return $this;
     }
@@ -39,5 +54,15 @@ class Error
     public function getMessage(): string
     {
         return $this->message;
+    }
+
+    public function getFile(): string|null
+    {
+        return $this->file;
+    }
+
+    public function getLine(): int|null
+    {
+        return $this->line;
     }
 }
